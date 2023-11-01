08cb6eee57d0e79f4c9eb2241d419091e2bb614a
thadien665 <marcinlejman665@gmail.com>
2023-11-01 18:19:38 +0100

    1st test done

--- test_case_1.py
+++ test_case_1.py
@@ -1,9 +1,13 @@
 from playwright.sync_api import sync_playwright
 from objects import login_page
 
-playwright = sync_playwright().start()
-browser = playwright.chromium.launch(headless = False)
-context = browser.new_context()
-page = context.new_page()
-page.goto(login_page.url)
-print(login_page.url.title())
+def test_login_1():
+    playwright = sync_playwright().start()
+    browser = playwright.chromium.launch()
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(login_page.url)
+    page.get_by_placeholder('Username').fill('standard_user')
+    page.get_by_placeholder('Password').fill('secret_sauce')
+    page.get_by_text('Login').click()
+    browser.close()
