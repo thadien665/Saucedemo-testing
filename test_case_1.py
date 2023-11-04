84a1bcf7002c23be1f6960d457fb017db2e095f5
thadien665 <marcinlejman665@gmail.com>
2023-11-04 17:32:13 +0100

    added 2nd test

--- test_case_1.py
+++ test_case_1.py
@@ -1,13 +1,26 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, expect
 from objects import login_page
 
-def test_login_1():
+def test_t01():
     playwright = sync_playwright().start()
-    browser = playwright.chromium.launch()
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto(login_page.url)
     page.get_by_placeholder('Username').fill('standard_user')
     page.get_by_placeholder('Password').fill('secret_sauce')
     page.get_by_text('Login').click()
+    expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
+    browser.close()
+
+def test_t02():
+    playwright = sync_playwright().start()
+    browser =  playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto(login_page.url)
+    page.get_by_placeholder('Username').fill('standard_user')
+    page.get_by_placeholder('Password').fill('secret')
+    page.get_by_text('Login').click()
+    expect(page.get_by_text('Epic sadface: username and password do not match any user in this service')).to_be_visible()
     browser.close()
